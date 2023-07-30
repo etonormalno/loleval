c01295cd17c48f63f9bdd9df7cc0d36fab1cef4f
etonormalno <86052185+etonormalno@users.noreply.github.com>
2023-07-30 23:31:16 +0300

    Update main.lua

--- library/main.lua
+++ library/main.lua
@@ -52,7 +52,7 @@ function zigzag(X) return math.acos(math.cos(X*math.pi))/math.pi end
 counter = 0
 
 while wait(0.1)do
- AccentColor = Color3.fromHSV(zigzag(counter),1,1)
+ Library.AccentColor = Color3.fromHSV(zigzag(counter),1,1)
  
  counter = counter + 0.01
 end
